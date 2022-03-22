5cfde72d60ccfa9809c7baba97d114daed3b6530
Juan Oliveira <jfs.dev.eng@gmail.com>
2022-03-22 08:48:05 -0300

    restful controller

--- src/main/java/com/example/spring_api_restful/rest/controller/ClienteController.java
+++ src/main/java/com/example/spring_api_restful/rest/controller/ClienteController.java
@@ -3,16 +3,21 @@ package com.example.spring_api_restful.rest.controller;
 import com.example.spring_api_restful.domain.entity.Cliente;
 import com.example.spring_api_restful.domain.repository.ClientesRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -41,10 +46,49 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/salvar")
+    @GetMapping
+    public ResponseEntity find(Cliente filtro){
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> listaCliente = clientesRepository.findAll(example);
+        return ResponseEntity.ok(listaCliente);
+    }
+
+    @PostMapping
     @ResponseBody
     public ResponseEntity salvarCliente(@RequestBody Cliente cliente){
         Cliente clienteSalvo = clientesRepository.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity updateCliente(@PathVariable("id") Integer id,
+                                        @RequestBody Cliente cliente){
+
+        return clientesRepository.findById(id)
+                .map( clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientesRepository.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet( () -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletarCliente(@PathVariable("id") Integer id) {
+
+        Optional<Cliente> cliente = clientesRepository.findById(id);
+
+        if (cliente.isPresent()) {
+            clientesRepository.deleteById(cliente.get().getId());
+
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
